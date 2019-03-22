c2bd5ff7883dbb4b0db27a2dfb5e94196bbe6ad9
FLAHOU Maxime <maxime.flahou@hcuge.ch>
2019-03-22 17:58:44 +0100

    :O c==3

---
+++ src/app/user-profile/user-profile.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+import { UserProfilePage } from './user-profile.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: UserProfilePage
+      }
+    ])
+  ],
+  declarations: [UserProfilePage]
+})
+export class UserProfilePageModule {}
