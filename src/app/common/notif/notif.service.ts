2f778e1f41929ad04e5f6853152e062376c7dcec
rtrm <rtrm@hcuge.ch>
2019-03-25 13:58:20 +0100

    test

--- src/app/common/notif/notif.service.ts
+++ src/app/common/notif/notif.service.ts
@@ -17,7 +17,8 @@ export class NotifService {
 
     public listen(): Observable<Transaction> {
         return Observable.create((observer) => {
-            const eventSource = new EventSource(`${environment.api}/transactions/sse-interpreter/1`);
+            const test = new Date().getTime();
+            const eventSource = new EventSource(`${environment.api}/transactions/sse-interpreter/${test}`);
             eventSource.onmessage = (event) => observer.next(this.onMessage(event));
             eventSource.onerror = (error) => {
                 console.error(error);
