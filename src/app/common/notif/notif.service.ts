6f1bcdb76e1dc6476aeb8dd78b6e7d55a654032d
rtrm <rtrm@hcuge.ch>
2019-03-25 13:52:49 +0100

    revert

--- src/app/common/notif/notif.service.ts
+++ src/app/common/notif/notif.service.ts
@@ -1,40 +1,35 @@
 import { Injectable, NgZone } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Transaction } from '../transaction/transaction.model';
 
 @Injectable({ providedIn: 'root' })
 export class NotifService {
     public notifs: Transaction[] = [];
-    public notif$: Observable<Transaction>;
+    public notif$ = new Subject<Transaction>();
 
     private zone = new NgZone({ enableLongStackTrace: false });
 
     constructor() {
-        this.notif$ = this.listen();
-        this.notif$.subscribe();
-    }
-
-    public listen(): Observable<Transaction> {
-        return Observable.create((observer) => {
-            const eventSource = new EventSource(`${environment.api}/transactions/sse-interpreter/1`);
-            eventSource.onmessage = (event) => observer.next(this.onMessage(event));
-            eventSource.onerror = (error) => observer.error(error);
-        });
+        const source = new EventSource(`${environment.api}/transactions/sse-interpreter/1`);
+        source.onerror = (err) => {
+            this.notif$.error(err);
+        };
+        source.onmessage = this.onMessage.bind(this);
     }
 
     private onMessage = (message: any) => {
         const tr: Transaction = JSON.parse(message.data);
         console.log(message);
+        // const tr = message.data;
         if (tr.state === 'PENDING') {
             this.notifs.push(tr);
-            return tr;
         } else {
             const tmp = this.notifs.findIndex((t) => t.id === tr.id);
             if (tmp > -1) {
                 this.notifs.splice(tmp, 1);
             }
-            return tr;
         }
+        this.zone.run(() => this.notif$.next(tr));
     }
 }
