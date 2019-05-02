c39e7dce44bfbb6b899665f4a29b954bcc36d6b9
rtrompier <remy.trompier@gmail.com>
2019-05-02 15:09:43 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://192.168.142.43:8080',
+  api: 'http://remy-trompier.ch:8123',
   KEYCLOAK_URL: 'http://remy-trompier.ch:9000/auth',
   KEYCLOAK_REALM: 'test',
   KEYCLOAK_CLIENT: 'comprehensio',
